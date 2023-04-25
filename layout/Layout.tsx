4f1f666cdf8d0ba6c1685822934750284e4aaa03
Ersultan <example@gmail.com>
2023-04-25 11:09:50 +0300

    Вёрстка меню

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,35 +1,33 @@
-import { LayoutProps } from "./Layout.props";
-import styles from "./P.module.css";
-import cn from "classnames";
-import { Header } from "./Header/Header";
-import React, { FunctionComponent } from "react";
-import { Sidebar } from "./Sidebar/Sidebar";
-import { Footer } from "./Footer/Footer";
-import { AppContextProvider, IAppContext } from "../context/app.context";
+import { LayoutProps } from './Layout.props';
+import styles from './Layout.module.css';
+import cn from 'classnames';
+import { Header } from './Header/Header';
+import React, { FunctionComponent } from 'react';
+import { Sidebar } from './Sidebar/Sidebar';
+import { Footer } from './Footer/Footer';
+import { AppContextProvider, IAppContext } from '../context/app.context';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
-  return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>
+				{children}
+			</div>
+			<Footer className={styles.footer} />
+		</div>
+	);
 };
 
-export const withLayout = <T extends Record<string, unknown> & IAppContext>(
-  Component: FunctionComponent<T>
-) => {
-  return function withLayoutComponent(props: T): JSX.Element {
-    return (
-      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
-        <Layout>
-          <Component {...props} />
-        </Layout>
-      </AppContextProvider>
-    );
-  };
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
+	return function withLayoutComponent(props: T): JSX.Element {
+		return (
+			<AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			</AppContextProvider>
+		);
+	};
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,46 @@
-import { GetStaticProps } from "next";
-import React, { useState } from "react";
-import { Button, Htag, P, Rating, Tag } from "../components";
-import { withLayout } from "../layout/Layout";
-import axios from "axios";
-import { MenuItem } from "../interfaces/menu.interface";
+import { GetStaticProps } from 'next';
+import React, { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
+import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
 function Home({ menu }: HomeProps): JSX.Element {
-  const [rating, setRating] = useState<number>(4);
+	const [rating, setRating] = useState<number>(4);
 
-  return (
-    <>
-      <Htag tag="h1">Заголовок</Htag>
-      <Button appearance="primary" arrow="right">
-        Кнопка
-      </Button>
-      <Button appearance="ghost" arrow="down">
-        Кнопка
-      </Button>
-      <P size="l">Большой</P>
-      <P>Средний</P>
-      <P size="s">Маленький</P>
-      <Tag size="s">Ghost</Tag>
-      <Tag size="m" color="red">
-        Red
-      </Tag>
-      <Tag size="s" color="green">
-        Green
-      </Tag>
-      <Tag color="primary">Green</Tag>
-      <Rating rating={rating} isEditable setRating={setRating} />
-     
-    </>
-  );
+	return (
+		<>
+			<Htag tag='h1'>Заголовок</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
+			<Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
+		</>
+	);
 }
 
 export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(
-    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
-    {
-      firstCategory,
-    }
-  );
-  return {
-    props: { 
-      menu,
-      firstCategory,
-    },
-  };
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
 };
 
 interface HomeProps extends Record<string, unknown> {
-  menu: MenuItem[];
-  firstCategory: number;
+	menu: MenuItem[];
+	firstCategory: number;
 }
