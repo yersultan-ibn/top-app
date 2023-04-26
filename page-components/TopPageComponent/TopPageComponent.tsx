54dd587bcb1a0bff314f5d2e74dcce652dc0b46b
Ersultan <example@gmail.com>
2023-04-26 22:26:02 +0300

    Вёрстка страницы продуктов - 2

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,38 +1,33 @@
-import { Card, HhData, Htag, Tag } from "../../components";
-import { TopPageComponentProps } from "./TopPageComponent.props";
-import styles from "./TopPageComponent.module.css";
-import { TopLevelCategory } from "../../interfaces/page.interface";
-export const TopPageComponent = (
-  { page, products, firstCategory },
-  TopPageComponentProps
-): JSX.Element => {
-  return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.title}>
-          <Htag tag="h1">{page.title}</Htag>
-          {products && (
-            <Tag color="grey" size="m">
-              {products.length}
-            </Tag>
-          )}
-          <span>Сортировка</span>
-        </div>
-        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
-        <div className={styles.hhTitle}>
-          {" "}
-          <Htag tag="h2">Вакансии - {page.category}</Htag>
-          <Tag color="red" size="m">
-            hh.ru
-          </Tag>
-          <Htag tag="h2">Вакансии - {page.category}</Htag>
-          <Tag color="red" size="m">
-            hh.ru
-          </Tag>
-        </div>
+import { Advantages, Htag, P, Tag } from '../../components';
+import { TopPageComponentProps } from './TopPageComponent.props';
+import styles from './TopPageComponent.module.css';
+import { HhData } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
 
-        {firstCategory === TopLevelCategory.Courses && <HhData {...page.hh} />}
-      </div>
-    </>
-  );
+export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.title}>
+				<Htag tag='h1'>{page.title}</Htag>
+				{products && <Tag color='grey' size='m'>{products.length}</Tag>}
+				<span>Сортировка</span>
+			</div>
+			<div>
+				{products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+			</div>
+			<div className={styles.hhTitle}>
+				<Htag tag='h2'>Вакансии - {page.category}</Htag>
+				<Tag color='red' size='m'>hh.ru</Tag>
+			</div>
+			{firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+			{page.advantages && page.advantages.length > 0 && <>
+				<Htag tag='h2'>Преимущства</Htag>
+				<Advantages advantages={page.advantages} />
+			</>
+			}
+			{page.seoText && <P>{page.seoText}</P>}
+			<Htag tag='h2'>Получаемые навыки</Htag>
+			{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
+		</div>
+	);
 };
