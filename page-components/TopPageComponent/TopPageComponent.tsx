8325d1713b3496f246a99562eb1b2cafc5861020
Ersultan <example@gmail.com>
2023-04-26 20:20:16 +0300

    Компоненты страниц

---
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -0,0 +1,8 @@
+import { TopPageComponentProps } from './TopPageComponent.props';
+export const TopPageComponent = ({page, products, firstCategory}, TopPageComponentProps):JSX.Element=> {
+return (
+    <>
+    {products && products.length}
+    </>
+)
+}
