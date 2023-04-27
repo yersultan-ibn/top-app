ff1c3484cb337f8c22bbab351b7dba0da463a562
Ersultan <example@gmail.com>
2023-04-27 18:25:40 +0300

    Компонент поиска

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -3,12 +3,13 @@ import styles from "./Sidebar.module.css";
 import cn from "classnames";
 import { Menu } from "../Menu/Menu";
 import Logo from "../logo.svg";
+import Search from "../../pages/search";
 
-export const Sidebar = ({className, ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
     <div {...props} className={cn(className, styles.sidebar)}>
-      <Logo  className={styles.logo}/>
-      <div>поиск</div>
+      <Logo className={styles.logo} />
+      <Search />
       <Menu />
     </div>
   );
