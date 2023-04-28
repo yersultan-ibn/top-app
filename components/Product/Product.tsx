e61dc1c1876d603192fa87324dffb292dbed0885
Ersultan <example@gmail.com>
2023-04-28 16:56:02 +0300

    Компонент Image

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -8,10 +8,21 @@ import { Button } from '../Button/Button';
 import { priceRu } from '../../helpers/helpers';
 import { Divider } from '../Divider/Divider';
 
+import Image from 'next/image';
+ 
+
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
-			<div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
+			<div className={styles.logo}>
+				<Image
+				src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+				alt={product.title}
+				width={70}
+				height={70}
+				/>				
+			<img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+			</div>
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
 				{priceRu(product.price)}
