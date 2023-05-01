c1c17088fa007107370a8aee4f8ffc50b2d2d05b
Ersultan <ersultansansyzbaj012@gmail.com>
2023-05-01 20:55:09 +0600

    Обработка ошибок

--- components/Textarea/Textarea.props.ts
+++ components/Textarea/Textarea.props.ts
@@ -1,7 +1,10 @@
 import { DetailedHTMLProps, TextareaHTMLAttributes } from "react";
+import { FieldError } from 'react-hook-form';
 
 export interface TextareaProps
   extends DetailedHTMLProps<
     TextareaHTMLAttributes<HTMLTextAreaElement>,
     HTMLTextAreaElement
-  > {}
+  > {
+    error?: FieldError;
+  }
