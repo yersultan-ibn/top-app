34b459332f1c21638496d9a79cb870d5e8d6c974
Ersultan <example@gmail.com>
2023-05-02 18:48:55 +0300

    Упражнение - Исправление бага useReducer

--- page-components/TopPageComponent/sort.reducer.ts
+++ page-components/TopPageComponent/sort.reducer.ts
@@ -1,12 +1,12 @@
 import { SortEnum } from "../../components/Sort/Sort.props";
 import { ProductModel } from "../../interfaces/product.interface";
 
-export type SortActions = { type: SortEnum } | { type: SortEnum.Rating };
+export type SortActions = { type: SortEnum } | { type: SortEnum.Rating } | {type: 'reset', initialState: ProductModel[]};
 
 export interface SortReducerState {
   sort: SortEnum;
   products: ProductModel[];
-}
+} 
 
 export const sortReducer = (
   state: SortReducerState,
@@ -25,6 +25,11 @@ export const sortReducer = (
         sort: SortEnum.Price,
         products: state.products.sort((a, b) => (a.price > b.price ? -1 : 1)),
       };
+      case 'reset':
+        return{
+          sort: SortEnum.Rating,
+          products: action.initialState
+        }
     default:
       throw new Error("Неверный тип сортировки");
   }
