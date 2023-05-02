1f3b39a591cddbd6a3df88b8fcb9337d9846917e
Ersultan <example@gmail.com>
2023-05-02 21:53:48 +0300

    Добавление landmarks

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,18 +1,16 @@
 import styles from './Menu.module.css';
 import cn from 'classnames';
-import { useContext } from 'react';
+import { useContext, KeyboardEvent } from 'react';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { firstLevelMenu } from '../../helpers/helpers';
-import {motion} from 'framer-motion'
-
+import { motion } from 'framer-motion';
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
 	const router = useRouter();
-
 
 	const variants = {
 		visible: {
@@ -22,7 +20,7 @@ export const Menu = (): JSX.Element => {
 				staggerChildren: 0.1
 			}
 		},
-		hidden: {marginBottom: 0}
+		hidden: { marginBottom: 0 }
 	};
 
 	const variantsChildren = {
@@ -30,9 +28,8 @@ export const Menu = (): JSX.Element => {
 			opacity: 1,
 			height: 29
 		},
-		hidden: {opacity: 0, height: 0}
+		hidden: { opacity: 0, height: 0 }
 	};
-
 
 	const openSecondLevel = (secondCategory: string) => {
 		setMenu && setMenu(menu.map(m => {
@@ -43,12 +40,19 @@ export const Menu = (): JSX.Element => {
 		}));
 	};
 
+	const openSecondLevelKey = (key: KeyboardEvent, secondCategory: string) => {
+		if (key.code == 'Space' || key.code == 'Enter') {
+			key.preventDefault();
+			openSecondLevel(secondCategory);
+		}
+	};
+
 	const buildFirstLevel = () => {
 		return (
 			<>
 				{firstLevelMenu.map(m => (
 					<div key={m.route}>
-						<Link href={`/${m.route}`} legacyBehavior>
+						<Link href={`/${m.route}`}>
 							<a>
 								<div className={cn(styles.firstLevel, {
 									[styles.firstLevelActive]: m.id == firstCategory
@@ -74,15 +78,20 @@ export const Menu = (): JSX.Element => {
 					}
 					return (
 						<div key={m._id.secondCategory}>
-							<div className={styles.secondLevel} onClick={() => openSecondLevel(m._id.secondCategory)}>{m._id.secondCategory}</div>
-							<motion.div 
-                            layout
-							variants={variants}
-							initial={m.isOpened ? 'visible' : 'hidden'}
-							animate={m.isOpened ? 'visible' : 'hidden'}							
-							className={cn(styles.secondLevelBlock)}
+							<div
+								tabIndex={0}
+								onKeyDown={(key: KeyboardEvent) => openSecondLevelKey(key, m._id.secondCategory)}
+								className={styles.secondLevel}
+								onClick={() => openSecondLevel(m._id.secondCategory)}
+							>{m._id.secondCategory}</div>
+							<motion.div
+								layout
+								variants={variants}
+								initial={m.isOpened ? 'visible' : 'hidden'}
+								animate={m.isOpened ? 'visible' : 'hidden'}
+								className={cn(styles.secondLevelBlock)}
 							>
-								{buildThirdLevel(m.pages, menuItem.route)}
+								{buildThirdLevel(m.pages, menuItem.route, m.isOpened ?? false)}
 							</motion.div>
 						</div>
 					);
@@ -91,25 +100,25 @@ export const Menu = (): JSX.Element => {
 		);
 	};
 
-	const buildThirdLevel = (pages: PageItem[], route: string) => {
+	const buildThirdLevel = (pages: PageItem[], route: string, isOpened: boolean) => {
 		return (
 			pages.map(p => (
-			<motion.div  key={p._id} variants={variantsChildren}>
-				<Link href={`/${route}/${p.alias}`} legacyBehavior>
-				<a className={cn(styles.thirdLevel, {
-					[styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
-				})}>
-					{p.category}
-				</a>
-				</Link>
-			</motion.div>
+				<motion.div key={p._id} variants={variantsChildren}>
+					<Link href={`/${route}/${p.alias}`}>
+						<a tabIndex={isOpened ? 0 : -1} className={cn(styles.thirdLevel, {
+							[styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
+						})}>
+							{p.category}
+						</a>
+					</Link>
+				</motion.div>
 			))
 		);
 	};
 
 	return (
-		<div className={styles.menu}>
+		<nav className={styles.menu} role='navigation'>
 			{buildFirstLevel()}
-		</div>
+		</nav>
 	);
 };
