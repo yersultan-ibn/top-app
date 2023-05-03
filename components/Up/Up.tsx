f01b07997305aad6330d1f492192a8431f9a73e6
Ersultan <example@gmail.com>
2023-05-03 14:07:55 +0300

    Сборка контейнера Docker

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -1,33 +1,31 @@
-import styles from "./Up.module.css";
-import cn from "classnames";
-import UpIcon from "./up.svg";
-import { useScrollY } from "../../hooks/useScrollY";
-import { motion, useAnimation } from "framer-motion";
-import { useEffect } from "react";
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
+import { useScrollY } from '../../hooks/useScrollY';
+import { ButtonIcon } from '../ButtonIcon/ButtonIcon';
+import styles from './Up.module.css';
 
 export const Up = (): JSX.Element => {
-  const controls = useAnimation();
-  const y = useScrollY();
+	const controls = useAnimation();
+	const y = useScrollY();
 
-  useEffect(() => {
-    controls.start({ opacity: y / document.body.scrollHeight });
-  }, [y, controls]);
+	useEffect(() => {
+		controls.start({ opacity: y / document.body.scrollHeight });
+	}, [y, controls]);
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
+	};
 
-  return (
-    <motion.button
-      className={styles.up}
-      onClick={scrollToTop}
-      animate={controls}
-      initial={{ opacity: 0 }}
-    >
-      <UpIcon />
-    </motion.button>
-  );
+	return (
+		<motion.div
+			className={styles.up}
+			animate={controls}
+			initial={{ opacity: 0 }}
+		>
+			<ButtonIcon appearance='primary' icon='up' aria-label="Наверх" onClick={scrollToTop} />
+		</motion.div>
+	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,33 @@
-import { GetStaticProps } from 'next';
-import React, { useState } from 'react';
-import { Button, Htag, Input, P, Rating, Tag, Textarea } from '../components';
-import { withLayout } from '../layout/Layout';
-import axios from 'axios';
-import { MenuItem } from '../interfaces/menu.interface';
-import { API } from '../helpers/api';
+import { GetStaticProps } from "next";
+import React, { useState } from "react";
+import { Button, Htag, Input, P, Rating, Tag, Textarea } from "../components";
+import { withLayout } from "../layout/Layout";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
+import { API } from "../helpers/api";
 
 function Home({ menu }: HomeProps): JSX.Element {
-	const [rating, setRating] = useState<number>(4);
+  const [rating, setRating] = useState<number>(4);
 
-	return (
-		<>
-			<Htag tag='h1'>Заголовок</Htag>
-			<Button appearance='primary' arrow='right'>Кнопка</Button>
-			<Button appearance='ghost' arrow='down'>Кнопка</Button>
-			<P size='l'>Большой</P>
-			<P>Средний</P>
-			<P size='s'>Маленький</P>
-			<Tag size='s'>Ghost</Tag>
-			<Tag size='m' color='red'>Red</Tag>
-			<Tag size='s' color='green'>Green</Tag>
-			<Tag color='primary'>Green</Tag>
-			<Rating rating={rating} isEditable setRating={setRating} />
-			<Input placeholder='тест' />
-			<Textarea placeholder='тест area' />
-		</>
-	);
+  return <Htag tag="h1">Выберите курс в необходимой категории</Htag>;
 }
 
 export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-	const firstCategory = 0;
-	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
-		firstCategory
-	});
-	return {
-		props: {
-			menu,
-			firstCategory
-		}
-	};
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+    firstCategory,
+  });
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
 };
 
 interface HomeProps extends Record<string, unknown> {
-	menu: MenuItem[];
-	firstCategory: number;
+  menu: MenuItem[];
+  firstCategory: number;
 }
