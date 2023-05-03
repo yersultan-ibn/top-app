e69e1606ee7c1fe3c4a14d646ee8ccaf0782583f
Ersultan <example@gmail.com>
2023-05-03 12:59:59 +0300

    Добавление meta на страницу

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -9,13 +9,23 @@ import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { TopPageComponent } from '../../page-components';
 import { API } from '../../helpers/api';
-
+import Head from 'next/head';
+ 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-	return <TopPageComponent
+	return <>
+	<Head>
+		<title>{page.metaTitle}</title>
+		<meta name='description' content={page.metaDescription}/>
+		<meta property='og:title' name='description' content={page.megaTitle}/>
+		<meta property='og:description' name='description' content={page.megaTitle}/>
+		<meta property='og:type' content='article'/>
+	</Head>
+	<TopPageComponent
 		firstCategory={firstCategory}
 		page={page}
 		products={products}
-	/>;
+	/>
+	</>
 }
 
 export default withLayout(TopPage);
